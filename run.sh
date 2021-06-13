ecd7b9cf6afa68f384c12769e2ec464ffc9f2ca9
Frankie Yeung <frankieycy@gmail.com>
2021-06-13 12:24:57 +0800

    updates

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ -std=c++11 -I /usr/local/Cellar/eigen/3.3.9/include/eigen3/ main.cpp -o main
+g++ -std=c++11 -I /usr/local/Cellar/eigen/3.3.9/include/eigen3/ calc/main.cpp -o exe/main
 if [ $? -eq 0 ]; then
-	./main > run.log
+	./exe/main > run.log
 fi
