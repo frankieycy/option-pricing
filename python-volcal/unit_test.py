2d7d6eea44dbc5aab0d7af7619c1a481344facaf
Frankie Yeung <frankieycy@gmail.com>
2022-01-04 17:48:59 -0500

    calibration functions for Heston model

--- python-volcal/unit_test.py
+++ python-volcal/unit_test.py
@@ -5,6 +5,9 @@ from pricer import *
 plt.switch_backend("Agg")
 
 paramsBCC = {"meanRevRate": 1.15, "correlation": -0.64, "volOfVol": 0.39, "meanVol": 0.04, "currentVol": 0.04}
+paramsBCCkey = list(paramsBCC.keys())
+paramsBCCval = list(paramsBCC.values())
+paramsBnd = ((0,10), (-1,1), (0,1), (0,1), (0,1))
 
 def test_BlackScholesImpVol():
     vol = np.array([0.23,0.20,0.18])
@@ -128,7 +131,7 @@ def test_calcFwdVarCurve():
     plt.close()
 
 def test_HestonFFT():
-    vol = lambda logStrikes: np.array([CharFuncImpliedVol(HestonCharFunc(**paramsBCC),True)(k,1) for k in logStrikes]).reshape(-1)
+    vol = lambda logStrikes: np.array([CharFuncImpliedVol(HestonCharFunc(**paramsBCC),FFT=True)(k,1) for k in logStrikes]).reshape(-1)
     k = np.arange(-0.4,0.4,0.02)
     iv = vol(k)
     fig = plt.figure(figsize=(6,4))
@@ -140,6 +143,54 @@ def test_HestonFFT():
     plt.savefig("test_HestonSmileBCC_FFT.png")
     plt.close()
 
+def test_HestonSmileLewis():
+    vol = lambda logStrikes: np.array([CharFuncImpliedVolLewis(HestonCharFunc(**paramsBCC))(k,1) for k in logStrikes]).reshape(-1)
+    k = np.arange(-0.4,0.4,0.02)
+    iv = vol(k)
+    fig = plt.figure(figsize=(6,4))
+    plt.scatter(k, 100*iv, c='k', s=5)
+    plt.title("Heston 1-Year Smile (BCC Params)")
+    plt.xlabel("log-strike")
+    plt.ylabel("implied vol (%)")
+    fig.tight_layout()
+    plt.savefig("test_HestonSmileBCC_Lewis.png")
+    plt.close()
+
+def test_calibrateModelToCallPrice():
+    df = pd.read_csv("spxVols20170424.csv")
+    df = df.drop(df.columns[0], axis=1)
+    Texp = df["Texp"].unique()
+    params0 = paramsBCCval
+    xT = list()
+    for T in Texp:
+        dfT = df[df["Texp"]==T]
+        k = np.log(dfT["Strike"]/dfT["Fwd"]).to_numpy()
+        mid = (dfT["CallMid"]/dfT["Fwd"]).to_numpy()
+        x = calibrateModelToCallPrice(k,T,mid,HestonCharFunc,params0,paramsBCCkey,bounds=paramsBnd)
+        params0 = x.tolist()
+        xT.append([T]+x.tolist())
+        print(f"T={np.round(T,3)}", x)
+    xT = pd.DataFrame(xT, columns=["Texp"]+paramsBCCkey)
+    xT.to_csv("test_HestonCalibration.csv", index=False)
+
+def test_ImpVolFromHestonCalibration():
+    cal = pd.read_csv("test_HestonCalibration.csv")
+    df = pd.read_csv("spxVols20170424.csv")
+    df = df.drop(df.columns[0], axis=1)
+    Texp = df["Texp"].unique()
+    dfnew = list()
+    for T in Texp:
+        dfT = df[df["Texp"]==T].copy()
+        params = cal[cal["Texp"]==T][paramsBCCkey]
+        params = params.iloc[0].to_dict()
+        vol = lambda logStrikes: np.array([CharFuncImpliedVol(HestonCharFunc(**params),FFT=True)(k,1) for k in logStrikes]).reshape(-1)
+        k = np.log(dfT["Strike"]/dfT["Fwd"]).to_numpy()
+        iv = vol(k)
+        dfT["Fit"] = iv
+        dfnew.append(dfT)
+    dfnew = pd.concat(dfnew)
+    plotImpliedVol(dfnew, "test_HestonImpliedVol.png")
+
 if __name__ == '__main__':
     # test_BlackScholesImpVol()
     # test_HestonSmile()
@@ -149,4 +200,7 @@ if __name__ == '__main__':
     # test_calcSwapCurve()
     # test_LevSwapCurve()
     # test_calcFwdVarCurve()
-    test_HestonFFT()
+    # test_HestonFFT()
+    # test_HestonSmileLewis()
+    # test_calibrateModelToCallPrice()
+    test_ImpVolFromHestonCalibration()
