0a0a05557488d0388e248ab752a06b6fe6813fa3
Frankie Yeung <frankieycy@gmail.com>
2022-03-15 14:35:56 -0400

    code org

--- python-volcal/params.py
+++ python-volcal/params.py
@@ -3,14 +3,16 @@
 paramsBCC = {"meanRevRate": 1.15, "correlation": -0.64, "volOfVol": 0.39, "meanVar": 0.04, "currentVar": 0.04}
 paramsBCCkey = list(paramsBCC.keys())
 paramsBCCval = list(paramsBCC.values())
-paramsBCCbnd = ((0.01,10), (-0.99,0.99), (0.01,1), (0.01,1), (0.01,1))
+paramsBCCbnd = ((0.01,10), (-0.99,0.99), (0.01,2), (0.01,1), (0.01,1))
 # paramsBCCbnd = ((0,10), (-1,1), (0,10), (0.01,1), (0,1))
+
 #### Merton ####################################################################
 paramsMER = {"vol": 0.1, "jumpInt": 0.1, "jumpMean": -0.4, "jumpSd": 0.2}
 paramsMERkey = list(paramsMER.keys())
 paramsMERval = list(paramsMER.values())
 paramsMERbnd = ((0.01,1), (0,5), (-0.9,0.9), (0.01,2))
 # paramsMERbnd = ((0,1), (0,10), (-1,1), (0,10))
+
 #### VGamma ####################################################################
 # Ref: Madan, The Variance Gamma Process and Option Pricing
 # paramsVG = {"vol": 0.12, "drift": -0.14, "timeChgVar": 0.17}
@@ -18,6 +20,7 @@ paramsVG = {"vol": 0.095, "drift": -0.096, "timeChgVar": 0.288} # converged
 paramsVGkey = list(paramsVG.keys())
 paramsVGval = list(paramsVG.values())
 paramsVGbnd = ((0.01,1), (-1,1), (0.01,1))
+
 #### SVJ #######################################################################
 # Ref: Gatheral, Volatility Workshop VW2.pdf
 # Empirical fits:
@@ -33,6 +36,7 @@ paramsSVJkey = list(paramsSVJ.keys())
 paramsSVJval = list(paramsSVJ.values())
 # paramsSVJbnd = ((0.01,10), (-0.99,0), (0.01,1), (0.01,1), (0.01,1), (0,2), (-0.9,0.5), (0.01,2))
 paramsSVJbnd = ((0.01,5), (-0.99,0), (0.1,1), (0.005,1), (0.005,1), (0,2), (-0.5,0.5), (0.01,0.5))
+
 #### rHeston: Poor Man's Approx ################################################
 # Ref: Gatheral, Roughening Heston
 # paramsRHPM = {"hurstExp": 0.05, "correlation": -0.67, "volOfVol": 0.41, "currentVar": 0.01}
@@ -40,4 +44,5 @@ paramsRHPM = {"hurstExp": 0.05, "correlation": -0.520, "volOfVol": 0.216, "curre
 paramsRHPMkey = list(paramsRHPM.keys())
 paramsRHPMval = list(paramsRHPM.values())
 paramsRHPMbnd = ((0,0.3), (-0.99,0.99), (0.01,1), (0.01,1))
+
 #### rHeston: Pade Approx ######################################################
