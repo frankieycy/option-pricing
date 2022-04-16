2be1eb622e6ecfb34651be77d332046eaade2d82
Frankie Yeung <frankieycy@gmail.com>
2022-04-16 12:06:43 -0400

    minor updates to GenIvDatasetFromStdDf

--- python-volcal/option.py
+++ python-volcal/option.py
@@ -42,7 +42,7 @@ def StandardizeOptionsChainDataset(df, onDate):
 
 #### Implied Vol Dataset #######################################################
 
-def GenerateImpVolDatasetFromStdDf(df):
+def GenerateImpVolDatasetFromStdDf(df, Nntm=6):
     # Generate implied vol dataset from standardized options chain df
     # Fi,PVi are implied from put-call parity, for each maturity i
     # Columns: "Contract Name","Expiry","Texp","Put/Call","Strike","Bid","Ask"
@@ -61,7 +61,7 @@ def GenerateImpVolDatasetFromStdDf(df):
         dfTp = dfTp[Kp.isin(K)]
         dfTc['Mid'] = (dfTc['Bid']+dfTc['Ask'])/2 # add Mid col
         dfTp['Mid'] = (dfTp['Bid']+dfTp['Ask'])/2
-        if len(K) >= 6:
+        if len(K) >= Nntm:
             K = K.to_numpy()
             bidc = dfTc['Bid'].to_numpy()
             bidp = dfTp['Bid'].to_numpy()
@@ -70,14 +70,14 @@ def GenerateImpVolDatasetFromStdDf(df):
             midc = dfTc['Mid'].to_numpy()
             midp = dfTp['Mid'].to_numpy()
             mids = midc-midp # put-call spread
-            Kntm = K[np.argsort(np.abs(midc-midp))][:6] # ntm strikes
+            Kntm = K[np.argsort(np.abs(midc-midp))[:Nntm]] # ntm strikes
             i = np.isin(K,Kntm)
             def objective(params):
                 F,PV = params
                 return sum((mids[i]-PV*(F-K[i]))**2)
             opt = minimize(objective,x0=(np.mean(mids[i]+K[i]),1))
             F,PV = opt.x
-            print(f"{expiry} F={F} PV={PV}")
+            print(f"T={expiry.date()} F={F} PV={PV}")
             ivcb = BlackScholesImpliedVol(F,K,T,0,bidc/PV,"call")
             ivca = BlackScholesImpliedVol(F,K,T,0,askc/PV,"call")
             ivpb = BlackScholesImpliedVol(F,K,T,0,bidp/PV,"put")
