b5654b514d5efe69a2c1de63d14423ce40af322d
Frankie Yeung <frankieycy@gmail.com>
2022-05-12 01:50:14 -0400

    amPrx func

--- python-volcal/american.py
+++ python-volcal/american.py
@@ -3,23 +3,61 @@ import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def PriceAmericanOption(strike, maturity, spotPrice, forwardPrice, impliedVol, timeStep=0.0002):
+amPrx_Stree = dict()
+amPrx_Otree = dict()
+
+def PriceAmericanOption(spotPrice, forwardPrice, strike, maturity, riskFreeRate, impliedVol, optionType, timeSteps):
     # Price American option via Cox binomial tree (d = 1/u)
     # Assume continuous dividend, reflected in forward price
     # TO-DO: proportional/discrete dividend (difficult!)
-    pass
+    global amPrx_Stree, amPrx_Otree
+    S,F,K,T,r,sig,n = spotPrice,forwardPrice,strike,maturity,riskFreeRate,impliedVol,timeSteps
+    if optionType == 'call':
+        payoff = lambda St: np.maximum(St-K,0)
+    else:
+        payoff = lambda St: np.maximum(K-St,0)
+    dt = T/n
+    D = np.exp(-r*dt)
+    u = np.exp(sig*np.sqrt(dt))
+    d = 1/u
+    R = (F/S)**(1/n)
+    p = (R-d)/(u-d)
+    q = 1-p
+    Stag = 'S=%.3f,F=%.3f,r=%.3f,n=%d' % (S,F,r,n)
 
-def AmericanOptionImpliedVol(priceMkt, strike, maturity, spotPrice, forwardPrice, timeStep=0.0002):
+    # Tree caching
+    if Stag in amPrx_Stree:
+        Stree = amPrx_Stree[Stag]
+    else:
+        Stree = np.zeros((n+1,n+1))
+        for i in range(n+1):
+            Stree[i,:i+1] = S*u**np.arange(-i,i+1,2)
+        amPrx_Stree[Stag] = Stree
+    if n in amPrx_Otree:
+        Otree = amPrx_Otree[n]
+    else:
+        Otree = np.zeros((n+1,n+1))
+        amPrx_Otree[n] = Otree
+
+    Otree[-1] = payoff(Stree[-1])
+    for i in range(n-1,-1,-1):
+        Otree[i,:i+1] = np.maximum(D*(p*Otree[i+1,1:i+2]+q*Otree[i+1,0:i+1]), payoff(Stree[i,:i+1]))
+
+    return Otree[0,0]
+
+def AmericanOptionImpliedVol(spotPrice, forwardPrice, strike, maturity, riskFreeRate, priceMkt, optionType, timeSteps):
     # Implied flat volatility under Cox binomial tree
     pass
 
-def AmericanOptionImpliedForward(priceMktPut, priceMktCall, strike, maturity, spotPrice, timeStep=0.0002):
-    # Implied forward from ATM put/call prices
-    # Iterate on fwd price until put/call implied vols converge ATM
+def AmericanOptionImpliedForwardAndRate(spotPrice, strike, maturity, priceMktPut, priceMktCall, timeSteps):
+    # Implied forward & riskfree rate from ATM put/call prices
+    # Iterate on fwd price & rate until put/call implied vols converge ATM
     pass
 
-def DeAmericanizedOptionsChainDataset(df, spotPrice, timeStep=0.0002):
+def DeAmericanizedOptionsChainDataset(df, spotPrice, stepSize):
     # De-Americanization of listed option prices into European pseudo-prices
     # Return standardized options chain dataset with columns: "Contract Name","Expiry","Texp","Put/Call","Strike","Bid","Ask"
     # Routine: (1) implied fwd prices (2) back out implied vols (3) cast to European prices (4) standardize dataset
     pass
+
+PriceAmericanOption_vec = np.vectorize(PriceAmericanOption)
