8c8792ead07ef06af6b59b07f5a6670d6d6e2cf8
Frankie Yeung <frankieycy@gmail.com>
2023-11-19 15:38:00 -0500

    am add comments

--- research/american-option/american.py
+++ research/american-option/american.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from numba import njit
 from scipy.sparse import diags
-from scipy.sparse.linalg import splu,spsolve
+from scipy.sparse.linalg import splu, spsolve
 from scipy.interpolate import interp1d
 plt.switch_backend('Agg')
+
+#### Note ######################################################################
+# - Attributes in all classes are freely accessible, i.e. no encapsulation (getter/setter)
 
 #### Vol Surface ###############################################################
 
@@ -16,17 +19,17 @@ MAX_LVAR = 10
 
 class SviPowerLaw:
     def __init__(self, v0, v1, v2, k1, k2, rho, eta, gam):
-        self.v0  = v0
-        self.v1  = v1
-        self.v2  = v2
-        self.k1  = k1
-        self.k2  = k2
-        self.rho = rho
-        self.eta = eta
-        self.gam = gam
-        self.IVolFunc = self.ImpliedVolFunc()
-        self.LVolFunc = self.LocalVolFunc()
-        self.LVarFunc = self.LocalVarFunc()
+        self.v0  = v0  # base variance
+        self.v1  = v1  # short-term variance
+        self.v2  = v2  # long-term variance
+        self.k1  = k1  # short-term mean-reversion
+        self.k2  = k2  # long-term mean-reversion
+        self.rho = rho # smile asymmetry
+        self.eta = eta # skew magnitude
+        self.gam = gam # skew decay
+        self.IVolFunc = self.ImpliedVolFunc() # implied-vol function
+        self.LVolFunc = self.LocalVolFunc()   # local-vol function
+        self.LVarFunc = self.LocalVarFunc()   # local-var function
 
     def HestonTermStructureKernel(self):
         def w0(T):
@@ -90,53 +93,57 @@ class SviPowerLaw:
 #### American Option ###########################################################
 
 class Option:
-    def __init__(self, K, T, pc, ex, px=None, ivEu=None, ivAm=None):
-        self.K        = K
-        self.T        = T
-        self.pc       = pc # P or C
-        self.ex       = ex # E or A
-        self.px       = px
-        self.pxFunc   = None
-        self.ivEu     = ivEu # true European vol (curved local-vol)
+    def __init__(self, K, T, pc, ex, px=None, ivEu=None, ivAm=None, lvEu=None):
+        self.K        = K    # strike
+        self.T        = T    # expiry
+        self.pc       = pc   # P or C
+        self.ex       = ex   # E or A
+        self.px       = px   # price
+        self.pxFunc   = None # price as function of spot
+        self.ivEu     = ivEu # European vol (curved local-vol)
         self.ivAm     = ivAm # de-Americanized vol (flat local-vol)
-        self.exBdryEu = None
-        self.exBdryAm = None
-        self.pxGridEu = None
-        self.pxGridAm = None
-        self.gridX    = None
-        self.gridT    = None
+        self.lvEu     = lvEu # local-vol at (K,T)
+        self.exBdryEu = None # ex-boundary under local-vol
+        self.exBdryAm = None # ex-boundary under flat local-vol
+        self.pxGridEu = None # price-grid under local-vol
+        self.pxGridAm = None # price-grid under flat local-vol
+        self.gridX    = None # space-grid in log-spot
+        self.gridT    = None # time-grid in time-to-expiry
 
     def Payoff(self, S):
         return np.maximum((self.K-S) if self.pc=='P' else (S-self.K),0)
 
 class Spot:
     def __init__(self, S0, r, q, impVolFunc, locVolFunc, locVarFunc, div=None):
-        self.S0 = S0
-        self.r  = r
-        self.q  = q
-        self.IVolFunc = impVolFunc
-        self.LVolFunc = locVolFunc
-        self.LVarFunc = locVarFunc
-        self.div = div
+        self.S0 = S0 # initial spot
+        self.r  = r  # risk-free rate
+        self.q  = q  # dividend yield
+        self.IVolFunc = impVolFunc # implied-vol function
+        self.LVolFunc = locVolFunc # local-vol function
+        self.LVarFunc = locVarFunc # local-var function
+        self.div = div # TODO: discrete dividend, use pd.Series? relative/absolute?
+
+    def ForwardFunc(self, T):
+        return np.exp((self.r-self.q)*T)
 
 class LatticeConfig:
-    def __init__(self, S0, nX, nT, rangeX, rangeT, centerValue, center='strike', scheme='explicit', boundary='value', interpBdry=False):
-        self.S0          = S0
-        self.nX          = nX
-        self.nT          = nT
-        self.rangeX      = rangeX
-        self.rangeT      = rangeT
-        self.centerValue = centerValue
-        self.center      = center
-        self.scheme      = scheme
-        self.boundary    = boundary
-        self.interpBdry  = interpBdry
-        self.setRangeS()
-        self.adjustRangeX()
-        self.dX = (rangeX[1]-rangeX[0])/nX
-        self.dT = (rangeT[1]-rangeT[0])/nT
-        self.gridX = np.linspace(rangeX[0],rangeX[1],nX+1)
-        self.gridT = np.linspace(rangeT[0],rangeT[1],nT+1)
+    def __init__(self, S0, nX, nT, rangeX, rangeT, centerValue, center='strike', scheme='implicit', boundary='value', interpBdry=False):
+        self.S0          = S0          # initial spot
+        self.nX          = nX          # number of space-grids
+        self.nT          = nT          # number of time-grids
+        self.rangeX      = rangeX      # range of space-grid
+        self.rangeT      = rangeT      # range of time-grid
+        self.centerValue = centerValue # grid-center in spot (S0 or K)
+        self.center      = center      # grid-center (spot or strike)
+        self.scheme      = scheme      # PDE solver scheme (explicit, implicit or crank-nicolson)
+        self.boundary    = boundary    # PDE grid boundary method (value or gamma)
+        self.interpBdry  = interpBdry  # TODO: interp early ex-boundary
+        self.SetRangeS()
+        self.AdjustRangeX()
+        self.dX = (rangeX[1]-rangeX[0])/nX # space-grid size
+        self.dT = (rangeT[1]-rangeT[0])/nT # time-grid size
+        self.gridX = np.linspace(rangeX[0],rangeX[1],nX+1) # space-grid in log-spot
+        self.gridT = np.linspace(rangeT[0],rangeT[1],nT+1) # time-grid in time-to-expiry
 
     def XToS(self, X):
         return self.centerValue*np.exp(X)
@@ -144,10 +151,10 @@ class LatticeConfig:
     def SToX(self, S):
         return np.log(S/self.centerValue)
 
-    def setRangeS(self):
+    def SetRangeS(self):
         self.rangeS = (self.XToS(self.rangeX[0]),self.XToS(self.rangeX[1]))
 
-    def adjustRangeX(self):
+    def AdjustRangeX(self):
         x0 = self.SToX(self.S0)
         self.rangeX[0] = np.minimum(x0,self.rangeX[0])
         self.rangeX[1] = np.maximum(x0,self.rangeX[1])
@@ -173,6 +180,7 @@ class LatticePricer:
         varL   = np.minimum(varL,MAX_LVAR)
         pxGrid = np.zeros((len(t),len(x)))
         pxGrid[0] = option.Payoff(S)
+        exBdry = np.concatenate([[K],[config.rangeS[0] if option.pc=='P' else config.rangeS[1]]*(len(t)-1)])
         if config.boundary == 'value':
             if option.pc == 'P':
                 pxGrid[:,0]  = K*np.exp(-r*t)-S[0]*np.exp(-q*t)
@@ -181,7 +189,6 @@ class LatticePricer:
                 pxGrid[:,0]  = 0
                 pxGrid[:,-1] = S[-1]*np.exp(-q*t)-K*np.exp(-r*t)
             V0 = np.zeros(len(x)-2)
-            exBdry = np.concatenate([[K],[config.rangeS[0] if option.pc=='P' else config.rangeS[1]]*(len(t)-1)])
             for i in range(len(t)-1):
                 if config.scheme == 'explicit':
                     v = varL[i,1:-1]
@@ -189,7 +196,7 @@ class LatticePricer:
                     b = 1-r*dt-v*dt/dx**2
                     c = (r-q-v/2)*dt/(2*dx)+v*dt/(2*dx**2)
                     D = diags([a[1:],b,c[:-1]],[-1,0,1]).tocsc()
-                    pxGrid[i+1,1:-1] = np.matmul(D,pxGrid[i,1:-1])
+                    pxGrid[i+1,1:-1] = D@pxGrid[i,1:-1]
                     pxGrid[i+1,1]   += a[0]*pxGrid[i,0]
                     pxGrid[i+1,-2]  += c[-1]*pxGrid[i,-1]
                 elif config.scheme == 'implicit':
@@ -200,7 +207,11 @@ class LatticePricer:
                     D = diags([a[1:],b,c[:-1]],[-1,0,1]).tocsc()
                     V0[0]  = a[0]*pxGrid[i+1,0]
                     V0[-1] = c[-1]*pxGrid[i+1,-1]
-                    pxGrid[i+1,1:-1] = spsolve(D,pxGrid[i,1:-1]-V0)
+                    pxGrid[i+1,1:-1] = splu(D).solve(pxGrid[i,1:-1]-V0)
+                    # pxGrid[i+1,1:-1] = spsolve(D,pxGrid[i,1:-1]-V0)
+                elif config.scheme == 'crank-nicolson':
+                    # TODO: Crank-Nicolson scheme
+                    pass
                 if option.ex == 'A':
                     intrinsic = option.Payoff(S)
                     if option.pc == 'P':
@@ -212,6 +223,7 @@ class LatticePricer:
                         exBdry[i+1] = S[idxEx]
                         pxGrid[i+1][idxEx:] = intrinsic[idxEx:]
         elif config.boundary == 'gamma':
+            # TODO: zero-gamma boundary
             pass
         pxFunc = interp1d(x,pxGrid[-1],kind='cubic')
         px     = pxFunc(x0)
@@ -220,6 +232,7 @@ class LatticePricer:
         option.px       = px
         option.pxFunc   = pxFunc
         option.ivEu     = self.spot.IVolFunc(x0,T)
+        option.lvEu     = self.spot.LVolFunc(x0,T)
         option.exBdryEu = exBdry
         option.pxGridEu = pxGrid
         option.gridX    = x
@@ -232,7 +245,8 @@ class LatticePricer:
 #### Helper Functions ##########################################################
 
 def VolSurfaceMatrixToDataFrame(m, k, T, idx_name='Expiry', col_name='Log-strike'):
-    df = pd.DataFrame(m, index=T, columns=k)
+    # Cast vol surface matrix to pd.DataFrame
+    df = pd.DataFrame(m,index=T,columns=k)
     df.index.name = idx_name
     df.columns.name = col_name
     return df
