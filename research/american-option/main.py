49a1de9a0c72ac28794c8a2b2ffe6a3e99575294
Frankie Yeung <frankieycy@gmail.com>
2023-11-20 17:26:46 -0500

    11-20 noon updates

--- research/american-option/main.py
+++ research/american-option/main.py
@@ -18,9 +18,9 @@ def test_SviPowerLaw():
     sigL = svi.LVolFunc(kk,TT)
     sigI = VolSurfaceMatrixToDataFrame(sigI,k,T)
     sigL = VolSurfaceMatrixToDataFrame(sigL,k,T)
-    print('sigI')
+    print('---sigI---')
     print(sigI)
-    print('sigL')
+    print('---sigL---')
     print(sigL)
     PlotImpliedVol(sigI,sigL,xlim=(-1,1),ylim=(12,100),figname='out/impliedvol.png')
 
@@ -30,28 +30,42 @@ def test_LatticePricer():
     T  = 1
     S0 = 1
     r  = 0.05
-    nX = 200
-    nT = 200
+    nX = 1000
+    nT = 1000
     x0 = -2
     x1 = 2
-    O = Option(K,T,'P','A')
+    pc = 'C'
+    ex = 'E'
+    O = Option(K,T,pc,ex)
     S = Spot(S0,r,0,svi)
-    C = LatticeConfig(S0,nX,nT,[x0,x1],[0,T],K,scheme='implicit')
+    C = LatticeConfig(S0,'implicit')
     L = LatticePricer(S)
+    C.initGrid(nX,nT,[x0,x1],[0,T],K)
     L.SolveLattice(O,C)
     print(O)
     print(S)
     print(C)
     print(L)
-    print('C.rangeS')
+    print('---C.rangeS---')
     print(C.rangeS)
-    print('O.exBdryLV')
+    print('---O.exBdryLV---')
     print(O.exBdryLV)
-    print('O.pxGridLV')
+    print('---O.pxGridLV---')
     print(O.pxGridLV)
+    if ex == 'E':
+        D = np.exp(-r*T)
+        F = S.ForwardFunc(T)
+        V = BlackScholesPrice(O.ivLV,K,T,D,F,pc)
+        sig = BlackScholesImpliedVol(O.px,K,T,D,F,pc,O.ivLV)[0]
+        print('---BSPrice---')
+        print('pxTrue',V,'pxLatt',O.px)
+        print('sigTrue',O.ivLV,'sigLatt',sig)
 
 def test_DeAmericanize():
-    svi = SviPowerLaw(**SVI_PARAMS_SPX)
+    # sigI and sigA should converge for nX and nT large enough
+    # Implicit scheme requires nT~1000
+    # Can Crank-Nicolson achieve accuracy with nT~200?
+    svi = SviPowerLaw(**SVI_PARAMS_FLAT)
     K  = 1
     T  = 1
     S0 = 1
@@ -60,17 +74,34 @@ def test_DeAmericanize():
     nT = 200
     x0 = -2
     x1 = 2
-    O = Option(K,T,'P','A')
+    pc = 'P'
+    ex = 'E'
+    O = Option(K,T,pc,ex)
     S = Spot(S0,r,0,svi)
-    C = LatticeConfig(S0,nX,nT,[x0,x1],[0,T],K,scheme='implicit')
+    C = LatticeConfig(S0,'implicit')
     L = LatticePricer(S)
+    F = S.ForwardFunc(T)
+    k = np.log(K/F)
+    C.initGrid(nX,nT,[x0,x1],[0,T],K)
     L.SolveLattice(O,C)
     L.DeAmericanize(O,C)
     print(O)
-    print('O.exBdryLV')
+    print(S)
+    print(C)
+    print(L)
+    print('---O.exBdryLV---')
     print(O.exBdryLV)
-    print('O.exBdryFV')
+    print('---O.exBdryFV---')
     print(O.exBdryFV)
+    print('---svi.IVol---')
+    print(svi.IVolFunc(k,T))
+    print('---svi.LVol---')
+    print(svi.LVolFunc(k,T))
+    if ex == 'E':
+        D = np.exp(-r*T)
+        sig = BlackScholesImpliedVol(O.px,K,T,D,F,pc,O.ivLV)[0]
+        print('---BSVol---')
+        print('sigTrue',O.ivLV,'sigDeAm',O.ivFV,'sigLatt',sig)
 
 def test_AmericanVolSurface():
     svi = SviPowerLaw(**SVI_PARAMS_SPX)
@@ -83,20 +114,121 @@ def test_AmericanVolSurface():
     x0 = -2
     x1 = 2
     S = Spot(S0,r,0,svi)
-    C = LatticeConfig(S0,nX,nT,[x0,x1],[0,T],K,scheme='implicit')
-    A = AmericanVolSurface(S,C)
+    C = LatticeConfig(S0,'implicit')
+    A = AmericanVolSurface(S,C,nX,nT,[x0,x1])
     k = np.arange(-0.5,0.6,0.1)
     sigI = svi.IVolFunc(k,T)
     sigA = A.IVolFunc(k,T)
-    print('sigI')
+    print('---sigI---')
     print(pd.Series(sigI,index=k))
-    print('sigA')
+    print('---sigA---')
     print(pd.Series(sigA,index=k))
-    print('A.log')
+    print('---A.log---')
     print(A.log)
+
+def test_LatticePricerAccuracy_FlatVol():
+    svi = SviPowerLaw(**SVI_PARAMS_FLAT)
+    S0 = 1
+    r  = 0.05
+    nX = 1000
+    nT = 1000
+    x0 = -2
+    x1 = 2
+    ex = 'E'
+    kk = np.arange(-0.5,0.55,0.05)
+    TT = np.arange(0.1,1.1,0.1)
+    m  = 'implicit'
+    S  = Spot(S0,r,0,svi)
+    C  = LatticeConfig(S0,m)
+    L  = LatticePricer(S)
+    with open(f'out/lattice_eu_acc_flatvol_nX={nX}_nT={nT}.csv','w') as f:
+        f.write(f'#S0={S0},r={r},nX={nX},nT={nT},x0={x0},x1={x1},m={m},vs={svi}\n')
+        f.write('k,K,T,ex,pc,pxTrue,pxLatt,sigTrue,sigLatt,sigLV,sigErr\n')
+        for T in TT:
+            T = round(T,2)
+            print(f'Running lattice at T={T} ...')
+            D = np.exp(-r*T)
+            F = S.ForwardFunc(T)
+            for k in tqdm(kk):
+                K  = F*np.exp(k)
+                pc = 'P' if k<=0 else 'C'
+                O  = Option(K,T,pc,ex)
+                C.initGrid(nX,nT,[x0,x1],[0,T],K)
+                L.SolveLattice(O,C)
+                pxTrue  = BlackScholesPrice(O.ivLV,K,T,D,F,pc)
+                pxLatt  = O.px
+                sigTrue = O.ivLV
+                sigLatt = BlackScholesImpliedVol(O.px,K,T,D,F,pc,O.ivLV)[0]
+                sigLV   = O.lvLV
+                sigErr  = sigLatt-sigTrue
+                f.write(f'{k},{K},{T},{ex},{pc},{pxTrue},{pxLatt},{sigTrue},{sigLatt},{sigLV},{sigErr}\n')
+
+def test_LatticePricerAccuracy_SpxVol():
+    svi = SviPowerLaw(**SVI_PARAMS_SPX)
+    S0 = 1
+    r  = 0.05
+    nX = 1000
+    nT = 1000
+    x0 = -2
+    x1 = 2
+    ex = 'E'
+    kk = np.arange(-0.5,0.52,0.02)
+    TT = np.arange(0.1,1.1,0.1)
+    m  = 'implicit'
+    S  = Spot(S0,r,0,svi)
+    C  = LatticeConfig(S0,m)
+    L  = LatticePricer(S)
+    with open(f'out/lattice_eu_acc_spxvol_nX={nX}_nT={nT}.csv','w') as f:
+        f.write(f'#S0={S0},r={r},nX={nX},nT={nT},x0={x0},x1={x1},m={m},vs={svi}\n')
+        f.write('k,K,T,ex,pc,pxTrue,pxLatt,sigTrue,sigLatt,sigLV,sigErr\n')
+        for T in TT:
+            T = round(T,2)
+            print(f'Running lattice at T={T} ...')
+            D = np.exp(-r*T)
+            F = S.ForwardFunc(T)
+            for k in tqdm(kk):
+                K  = F*np.exp(k)
+                pc = 'P' if k<=0 else 'C'
+                O  = Option(K,T,pc,ex)
+                C.initGrid(nX,nT,[x0,x1],[0,T],K)
+                L.SolveLattice(O,C)
+                pxTrue  = BlackScholesPrice(O.ivLV,K,T,D,F,pc)
+                pxLatt  = O.px
+                sigTrue = O.ivLV
+                sigLatt = BlackScholesImpliedVol(O.px,K,T,D,F,pc,O.ivLV)[0]
+                sigLV   = O.lvLV
+                sigErr  = sigLatt-sigTrue
+                f.write(f'{k},{K},{T},{ex},{pc},{pxTrue},{pxLatt},{sigTrue},{sigLatt},{sigLV},{sigErr}\n')
+
+def test_LatticePricer_ATMAmPut():
+    svi = SviPowerLaw(**SVI_PARAMS_SPX)
+    K  = 1
+    T  = 1
+    S0 = 1
+    r  = 0.05
+    nX = 1000
+    nT = 1000
+    x0 = -2
+    x1 = 2
+    pc = 'P'
+    ex = 'A'
+    O = Option(K,T,pc,ex)
+    S = Spot(S0,r,0,svi)
+    C = LatticeConfig(S0,'implicit')
+    L = LatticePricer(S)
+    C.initGrid(nX,nT,[x0,x1],[0,T],K)
+    L.SolveLattice(O,C)
+
+def test_LatticePricer_ATMAmCall():
+    pass
+
+def test_LatticePricer_CrankNicolson():
+    pass
 
 if __name__ == '__main__':
     # test_SviPowerLaw()
     # test_LatticePricer()
     # test_DeAmericanize()
-    test_AmericanVolSurface()
+    # test_AmericanVolSurface()
+    # test_LatticePricerAccuracy_FlatVol()
+    test_LatticePricerAccuracy_SpxVol()
